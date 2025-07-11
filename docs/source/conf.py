bdb4e882df3113dad9773c5b7003cef4a3e24fdc
Dylan Wase <7753432+DylanW621@users.noreply.github.com>
2025-07-11 15:12:55 +0200

    Update conf.py with my details

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Expo link directory'
+copyright = '2025, Dylan Wase'
+author = 'Dylan Wase'
 
 release = '0.1'
 version = '0.1.0'
